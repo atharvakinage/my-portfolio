d417e1b604482ac957ae7eac9ce9a823609bc90c
atharvakinage <kinageatharv@gmail.com>
2024-12-24 15:50:30 +0530

    Update artworks page - added paintperl link

--- src/app/artworks/page.js
+++ src/app/artworks/page.js
@@ -27,6 +27,8 @@ export default function Artworks() {
         <div className="w-full md:w-1/2">
           <h2 className="text-xl font-semibold mb-2">Elephant</h2>
           <p className="text-gray-600">In nature's realm, the gentle giant strides, Majestic grace, soulful eyes, serenity abides.</p>
+          <br></br>
+          <a href="https://www.paintperl.com/post/elephant" className="text-gray-500 hover:text-blue-500">View on Paintperl</a>
         </div>
       </div>
 
@@ -49,6 +51,8 @@ export default function Artworks() {
         <div className="w-full md:w-1/2">
           <h2 className="text-xl font-semibold mb-2">Horse</h2>
           <p className="text-gray-600">Elegance in every flick of name with grace under its reins where freedom runs wild, a majestic beauty in every stride.</p>
+          <br></br>
+          <a href="https://www.paintperl.com/post/horse" className="text-gray-500 hover:text-blue-500">View on Paintperl</a>
         </div>
       </div>
 
@@ -60,9 +64,9 @@ export default function Artworks() {
             src="/snake.jpg" 
             alt="Snake" 
             layout="responsive" 
-            width={1200}  // Adjust according to image size
-            height={800}  // Adjust according to image size
-            objectFit="contain" // This ensures the full image is shown
+            width={1200}  
+            height={800}  
+            objectFit="contain"
             className="rounded-md"
           />
         </div>
@@ -71,7 +75,8 @@ export default function Artworks() {
         <div className="w-full md:w-1/2">
           <h2 className="text-xl font-semibold mb-2">Snake</h2>
           <p className="text-gray-600">Nature's sleek predator, they roam, The dance of scales, a whispered poem.</p>
-          
+          <br></br>
+          <a href="https://www.paintperl.com/post/snake" className="text-gray-500 hover:text-blue-500">View on Paintperl</a>
         </div>
       </div>
     </div>
