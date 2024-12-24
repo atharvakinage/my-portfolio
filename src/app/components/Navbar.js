bd4a17f5f50b1cb5d5492269f6f8f3b095f66ffc
atharvakinage <kinageatharv@gmail.com>
2024-12-24 18:46:35 +0530

    Update home page layout

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
             </Link>
           </div>
           <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
+            <div className="ml-auto flex items-baseline space-x-4">
               <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 Home
               </Link>
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex flex-col md:flex-row items-center justify-between">
       <div className="md:w-1/2 mb-8 md:mb-0">
         <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-        <p className="text-xl mb-6">I am a 5th-semester Computer Science Engineering student at PES University, dedicated to both academic achievement and extracurricular engagement. I am passionate about leveraging technology to address and solve complex problemsand driven by curiosity to explore innovative solutions. Outside of academics, I play volleyball at the state level and have a passion for sketching, which reflects my commitment to teamwork and creativity.</p>
+        <p className="text-xl text-justify mb-6">I am a 5th-semester Computer Science Engineering student at PES University, dedicated to both academic achievement and extracurricular engagement. I am passionate about leveraging technology to address and solve complex problemsand driven by curiosity to explore innovative solutions. Outside of academics, I play volleyball at the state level and have a passion for sketching, which reflects my commitment to teamwork and creativity.</p>
         <div className="flex space-x-4">
           <a href="https://github.com/atharvakinage" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-blue-500">
             <Github size={24} />
