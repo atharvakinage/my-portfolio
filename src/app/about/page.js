d09691a8af17c1d7eb62bcf542492dbd7edf6986
atharvakinage <kinageatharv@gmail.com>
2024-12-25 21:21:22 +0530

    Update about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,12 +6,30 @@ export default function About() {
           <div>
             <h2 className="text-2xl font-semibold mb-4">My Journey</h2>
             <p className="mb-4">Here you can write about your background, education, and professional journey.</p>
+
+            <div className="mb-6">
+            <h3 className="text-xl font-semibold mb-4">EDUCATION</h3>
+            <div className="space-y-4">
+              <div>
+                <h4 className="text-lg font-medium">B.Tech</h4>
+                <div className="text-gray-500">PES University | 2022 - 2026</div>
+                <div className="text-white-600">Computer Science Engineering (5th Sem)</div>
+                <div className="text-white-600">CGPA - 8.67</div>
+              </div>
+              <div>
+                <h4 className="text-gray-500">BGS NPS | 2013 - 2022</h4>
+                <div className="text-white-600">CBSE Class XII - 2022 (95.6%)</div>
+                <div className="text-white-600">CBSE Class X - 2020 (93.8%)</div>
+              </div>
+            </div>
+          </div>
+
             <h2 className="text-2xl font-semibold mb-4">Skills</h2>
             <ul className="list-disc list-inside">
-              <li>JavaScript</li>
-              <li>React</li>
-              <li>Next.js</li>
-              <li>Tailwind CSS</li>
+              <li>Programming: C, Python, C++, R</li>
+              <li>Frontend: HTML/CSS, Javascript</li>
+              <li>Database: MySQL, MongoDB</li>
+              <li>Familiar with MERN stack, next.js, OpenCV, Rust, Tailwind CSS</li>
             </ul>
           </div>
           <div>
